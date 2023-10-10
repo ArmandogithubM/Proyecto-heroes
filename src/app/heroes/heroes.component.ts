c6c66c5eb819e24d3caaba678fedd665125ecf55
ArmandogithubM <145710598+ArmandogithubM@users.noreply.github.com>
2023-10-10 09:27:47 -0700

    Agregar

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit {   
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+  
+  heroes = HEROES;
+  selectedHero: Hero={
+    id: 0,
+    name: ''
+  }
 
-  constructor() { }
-
+  
+  constructor() { 
+    selectedHero: HeroDetailComponent;
+  }
   ngOnInit(): void {
-
+  }
+  
+  onSelect(hero: Hero): void {    
+    this.selectedHero = hero;
   }
 }
